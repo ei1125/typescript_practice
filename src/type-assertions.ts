57bba35a4003e6149cf6fc03935d856ad1949233
ei1125 <i.dont.like.monday1125@gmail.com>
2020-06-07 17:12:39 +0900

    type-assertions

---
+++ src/type-assertions.ts
@@ -0,0 +1,8 @@
+export {};
+//型アサーション
+let name: any = 'Ham';
+// let length = name.length as number;
+// let length = (name as string).length;
+let length = (<string>name).length;
+
+// length = 'foo';
